f50ffaf78926a29329ea20c2de860c014815516d
joelocano <joelangelocano@gmail.com>
2024-05-15 13:26:08 -0500

    feat(frontend): ➕ initital commit

---
+++ client/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["@/assets/styles/global.css"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/color-mode"],
+  colorMode: {
+    classSuffix: "",
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
