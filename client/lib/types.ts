6add0c7c821d2e5a1820dbd3b40db7af62f2fac2
joelocano <joelangelocano@gmail.com>
2024-05-18 19:57:28 -0500

    feat(frontend): :sparkles: student carousel

---
+++ client/lib/types.ts
@@ -0,0 +1,8 @@
+export type StudentType = {
+  id: number;
+  studentName: string;
+  studentPhoto: any;
+  studentContact: string;
+  authorizedName: string;
+  authorizedContact: string;
+};
---
+++ client/pages/teacher/index.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { StudentCard } from '@/components/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel'
+import type { StudentType } from '@/lib/types'
+
+const students: StudentType[] = [
+  {
+    id: 1,
+    studentName: 'PEPITO RODRIGUEZ',
+    studentPhoto: 'student1',
+    studentContact: '222 222 222',
+    authorizedName: 'PEPITO RODRIGUEZ',
+    authorizedContact: '999 999 999'
+  },
+  {
+    id: 2,
+    studentName: 'PEPITA MARTÍNEZ',
+    studentPhoto: 'student2',
+    studentContact: '333 333 333',
+    authorizedName: 'JULITO RODRIGUEZ',
+    authorizedContact: '666 666 666'
+  }
+]
+
+</script>
+
+<template>
+  <div class="py-7 grid justify-center">
+    <Carousel
+      class="relative w-full grid items-center"
+      :opts="{
+        align: 'start',
+        loop: true,
+      }"
+    >
+      <CarouselPrevious class="left-0 top-1/2 grid !bg-transparent border-none" />
+      <CarouselContent>
+        <CarouselItem v-for="(student, index) in students" :key="index">
+          <StudentCard :data="student" />
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselNext class="right-0 top-1/2 grid !bg-transparent border-none" />
+    </Carousel>
+  </div>
+</template>
