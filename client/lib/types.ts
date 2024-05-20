a91e66b4b31dfae0a6590888437cbb860af6f45e
joelocano <joelangelocano@gmail.com>
2024-05-20 16:37:00 -0500

    refactor(frontend): :art: added list on teacher view

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -1,8 +1,13 @@
+import type { StudentStatusEnum } from './enums'
+
 export type StudentType = {
   id: number;
   studentName: string;
   studentPhoto: any;
   studentContact: string;
+  studentGrade: string;
+  studentStatus: StudentStatusEnum;
   authorizedName: string;
   authorizedContact: string;
+  tutorContact: string;
 };
--- client/pages/teacher/index.vue
+++ client/pages/teacher/index.vue
@@ -1,51 +1,98 @@
 <script setup lang="ts">
-import { StudentCard } from '@/components/card'
+import { StudentCard } from './_components'
+import { Badge } from '@/components/ui/badge'
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious
-} from '@/components/ui/carousel'
+  Dialog,
+  DialogContent,
+  DialogTrigger
+} from '@/components/ui/dialog'
+import { StudentStatusEnum } from '@/lib/enums'
+
 import type { StudentType } from '@/lib/types'
 
 const students: StudentType[] = [
   {
     id: 1,
-    studentName: 'PEPITO RODRIGUEZ',
+    studentName: 'PEPITA MARTÍNEZ',
     studentPhoto: 'student1',
     studentContact: '222 222 222',
-    authorizedName: 'PEPITO RODRIGUEZ',
-    authorizedContact: '999 999 999'
+    studentGrade: '2B',
+    studentStatus: StudentStatusEnum.NO_WITHDRAWN,
+    authorizedName: 'JUANITO MOYA',
+    authorizedContact: '+54 9 3513786543',
+    tutorContact: '+54 9 3515987234'
+
   },
   {
     id: 2,
-    studentName: 'PEPITA MARTÍNEZ',
+    studentName: 'PEPITO RODRIGUEZ',
     studentPhoto: 'student2',
     studentContact: '333 333 333',
+    studentGrade: '2B',
+    studentStatus: StudentStatusEnum.NO_WITHDRAWN,
+    authorizedName: 'PEDRO MAPACHO',
+    authorizedContact: '666 666 666',
+    tutorContact: '+54 9 3515987234'
+  },
+  {
+    id: 3,
+    studentName: 'SANDRA MORÁN',
+    studentPhoto: 'student2',
+    studentContact: '333 333 333',
+    studentGrade: '2B',
+    studentStatus: StudentStatusEnum.WITHDRAWN,
     authorizedName: 'JULITO RODRIGUEZ',
-    authorizedContact: '666 666 666'
+    authorizedContact: '666 666 666',
+    tutorContact: '+54 9 3515987234'
+  },
+  {
+    id: 3,
+    studentName: 'JULIO VANEGAS',
+    studentPhoto: 'student2',
+    studentContact: '333 333 333',
+    studentGrade: '2B',
+    studentStatus: StudentStatusEnum.NO_ATTEND,
+    authorizedName: 'JULITO RODRIGUEZ',
+    authorizedContact: '666 666 666',
+    tutorContact: '+54 9 3515987234'
+  },
+  {
+    id: 4,
+    studentName: 'JUAN BIANCHI',
+    studentPhoto: 'student2',
+    studentGrade: '2B',
+    studentContact: '333 333 333',
+    studentStatus: StudentStatusEnum.WITHDRAWN,
+    authorizedName: 'JULITO RODRIGUEZ',
+    authorizedContact: '666 666 666',
+    tutorContact: '+54 9 3515987234'
   }
 ]
 
 </script>
 
 <template>
-  <div class="py-7 grid justify-center">
-    <Carousel
-      class="relative w-full grid items-center"
-      :opts="{
-        align: 'start',
-        loop: true,
-      }"
-    >
-      <CarouselPrevious class="left-0 top-1/2 grid !bg-transparent border-none" />
-      <CarouselContent>
-        <CarouselItem v-for="(student, index) in students" :key="index">
+  <nav class="h-[40px] shadow-md w-full fixed left-0 top-0 bg-[#D9D9D9]" />
+
+  <div class="grid gap-4 pb-7 pt-16">
+    <Dialog v-for="(student, index) in students" :key="index">
+      <DialogTrigger as-child>
+        <div class="p-0 line-clamp-1 items-center grid gap-2.5 grid-cols-[1fr_110px]">
+          <Badge variant="default" class="text-xl h-[42px] bg-[#1D1B20]/30 shadow-lg text-foreground">
+            {{ student.studentName }}
+          </Badge>
+          <Badge
+            class="w-full text-base bg-[#1D1B20]/30 text-foreground justify-self-end"
+          >
+            {{ student.studentStatus }}
+          </Badge>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <div class="overflow-y-auto">
           <StudentCard :data="student" />
-        </CarouselItem>
-      </CarouselContent>
-      <CarouselNext class="right-0 top-1/2 grid !bg-transparent border-none" />
-    </Carousel>
+        </div>
+      </DialogContent>
+    </Dialog>
   </div>
 </template>
