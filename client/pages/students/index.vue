b69d30484fe0a7f2bcaa11fc66fa5b5c1069717f
Nicolas <nicolasgalarza40@gmail.com>
2024-05-21 08:29:30 -0300

    feat(frontend): :sparkles: director basic tables

---
+++ client/pages/students/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2 class="text-3xl my-5">Estudiantes</h2>
+    <Table :columns="columns" :data="data" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Table from '@/pages/students/_components/Table.vue';
+import { columns } from './_components/columns';
+import type { Student } from './_components/interfaces/student';
+
+const data: Student[] = [
+  {
+    id: 1,
+    firstName: 'Lautaro',
+    lastName: 'Ibarra',
+    dni: '230456789',
+    age: 8,
+    course: '2A'
+  },  
+  {
+    id: 2,
+    firstName: 'Sofía',
+    lastName: 'Osorio',
+    dni: '323456789',  
+    age: 8,
+    course: '2B'
+  },
+  {
+    id: 3,
+    firstName: 'Felipe',
+    lastName: 'Maidana',  
+    dni: '853456789',
+    age: 7, 
+    course: '1A'
+  },
+];
+</script>
---
+++ client/pages/tutors/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2 class="text-3xl my-5">Apoderados</h2>
+    <Table :columns="columns" :data="data" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Table from '@/pages/students/_components/Table.vue';
+import { columns } from './_components/columns';
+import type { Tutor } from './_components/interfaces/tutor';
+
+const data: Tutor[] = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    dni: "123456789",
+    email: "johndoe@example.com",
+    phone: "123-456-7890"
+  },  
+  {
+    id: 2,
+    firstName: "Gonzalo",
+    lastName: "Gutierrez",
+    dni: "123456789",
+    email: "gonzagutierrez@example.com",
+    phone: "312-456-7890"
+  },
+]
+</script>
