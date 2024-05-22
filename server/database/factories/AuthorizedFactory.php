55ea29e294e5799ccab7bb86f58deca40cce1082
Nicolas Galarza <65139326+nicogmrz4@users.noreply.github.com>
2024-05-22 13:05:14 -0300

    Revert "C1 mpv 66 pbi 9 crear vista directora"

---
+++ server/database/factories/AuthorizedFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Authorized>
+ */
+class AuthorizedFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->firstName,
+            'lastname' => $this->faker->lastName,
+            'documentnumber' => $this->faker->unique()->numerify('##########'),
+            'phone' => $this->faker->phoneNumber,
+            'photo' => 'user.png',
+            'tutor_id' => null,
+        ];
+    }
+}
