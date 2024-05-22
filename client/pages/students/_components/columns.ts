51f98b3d58232f179013d0a8215060e003b29b70
Nicolas <nicolasgalarza40@gmail.com>
2024-05-22 14:01:54 -0300

    feat(frontend): :sparkles: director views created with a table within each of them

---
+++ client/pages/students/_components/columns.ts
@@ -0,0 +1,31 @@
+import type { ColumnDef } from '@tanstack/vue-table'
+import type { Student } from './interfaces/student'
+
+export const columns: ColumnDef<Student>[] = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+  },
+  {
+    accessorKey: 'firstName',
+    header: 'Nombre',
+  },
+  {
+    accessorKey: 'lastName',
+    header: 'Apellido',
+  },
+  {
+    accessorKey: 'dni',
+    header: 'N° Documento',
+  },
+  {
+    accessorKey: 'age',
+    header: () => h('div', { class: 'text-center' }, 'Edad'),
+    cell: ({ row }) => h('div', { class: 'text-center' }, row.getValue('age')),
+  },  
+  {
+    accessorKey: 'course',
+    header: () => h('div', { class: 'text-center' }, 'Curso'),
+    cell: ({ row }) => h('div', { class: 'text-center' }, row.getValue('course')),
+  },
+]
---
+++ client/pages/teachers/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2 class="text-3xl my-5">Maestros</h2>
+    <Table :columns="columns" :data="data" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Table from '@/pages/teachers/_components/Table.vue';
+import { columns } from './_components/columns';
+import type { Teacher } from './_components/interfaces/teacher';
+
+const data: Teacher[] = [
+  {
+    id: 1,
+    firstName: 'Mariana',
+    lastName: 'Martinez',
+    email: 'marianamartinez@example.com',
+    dni: '123456789',
+    phone: '123-456-7890',
+    course: '2A'
+  },  
+  {
+    id: 2,
+    firstName: 'Jorge',
+    lastName: 'Olivera',  
+    email: 'jorgeolivera@example.com',
+    dni: '323456789',
+    phone: '123-456-7890',
+    course: '2B'
+  },
+  {
+    id: 3,
+    firstName: 'Eugenia',
+    lastName: 'Hernandez',  
+    email: 'eugeniahernandez@example.com',
+    dni: '66456789',
+    phone: '123-456-7890',
+    course: '1A'
+  },
+];
+</script>
