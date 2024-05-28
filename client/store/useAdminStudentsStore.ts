90295704b01b495b34b0f08a8baefaf98ce746ba
Nicolas <nicolasgalarza40@gmail.com>
2024-05-28 17:48:19 -0300

    feat(frontend): add stores students, teachers and tutors for admin view

---
+++ client/store/useAdminStudentsStore.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import type { ResponseApi } from "@/types/api/responseApi";
+import type { StudentApi } from "@/types/api/studentApi";
+import type { FilterApi } from "@/types/api/filters/filterApi";
+
+export const useAdminStudentsStore = defineStore("adminStudentsStore", () => {
+  const ENDPOINT = "/api/students";
+  const API_URL = useRuntimeConfig().public.baseApiUrl + ENDPOINT;
+
+  async function getStudents(
+    offset: number = 0,
+    limit: number = 10,
+    filters: FilterApi[] = [],
+  ) {
+    const res = await axios.get<ResponseApi<StudentApi>>(API_URL, {
+      params: { offset, limit, filters },
+    });
+
+    return res.data;
+  }
+
+  return {
+    getStudents,
+  };
+});
