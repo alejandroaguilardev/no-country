1d5e27f7a6ffdc278a212674c18c985af3808485
Nicolas <nicolasgalarza40@gmail.com>
2024-06-04 17:32:13 -0300

    feat(frontend): :zap: add navegation for director view

---
+++ client/components/nav/AdminNav.vue
@@ -0,0 +1,85 @@
+<template>
+  <nav
+    class="w-full bg-white shadow-md fixed top-0 left-0 flex items-center px-20 z-50"
+  >
+    <ul class="flex gap-2 items-end">
+      <li v-for="(item, i) in navItems" :key="i" class="flex box-content">
+        <NuxtLink
+          class="p-4 opacity-50 hover:opacity-100 box-content"
+          :to="item.url"
+          >{{ item.label }}</NuxtLink
+        >
+      </li>
+    </ul>
+    <div class="ml-auto">
+      <DropdownMenu v-model:open="currentUserDropdown">
+        <DropdownMenuTrigger
+          class="text-slate-800 text-sm flex gap-1 justify-center"
+        >
+          <CircleUserRound :size="18" />
+          {{ currentUser?.name }}
+          <ChevronDown
+            :size="18"
+            class="dropdown-arrow"
+            :style="currentUserDropdown ? 'transform: rotate(-180deg)' : ''"
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <!--          <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
+          <DropdownMenuSeparator /> -->
+          <DropdownMenuItem @click="onLogout">Cerrar sesión</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { ChevronDown, CircleUserRound } from "lucide-vue-next";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  // DropdownMenuLabel,
+  // DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuthStore } from "@/store/useAuthStore";
+import type { UserType } from "@/types/models";
+
+const currentUser: Ref<UserType | null> = ref(null);
+const currentUserDropdown: Ref<boolean> = ref(false);
+const navItems = [
+  {
+    label: "Maestros",
+    url: "/admin/teachers",
+  },
+  {
+    label: "Apoderados",
+    url: "/admin/tutors",
+  },
+  {
+    label: "Estudiantes",
+    url: "/admin/students",
+  },
+];
+
+const onLogout = () => {
+  useAuthStore().logout();
+};
+
+onMounted(() => {
+  const { user } = useAuthStore();
+  currentUser.value = user;
+});
+</script>
+
+<style scoped>
+.dropdown-arrow {
+  transition: all 0.2s ease-in-out;
+}
+
+.router-link-active {
+  @apply border-b-[4px] border-black opacity-100;
+}
+</style>
