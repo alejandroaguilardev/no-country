a98725ee7524d601d116b26279a57261335c1afe
Musagy <diegomusagy@gmail.com>
2024-06-11 23:11:27 -0500

    feat(frontend): :art: Update Login page UI

--- client/pages/login/index.vue
+++ client/pages/login/index.vue
@@ -12,9 +12,6 @@ import { Input } from "@/components/ui/input";
 import { type LoginReq } from "@/types/queries";
 import { useAuthStore } from "@/store/useAuthStore";
 
-const schoolInfo = {
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTRJlM7tWbKZYxzdthfmaF9ndkO4CH7peOI95Er9jFgA&s",
-};
 const mainHeight = "lg:h-screen lg:h-[100dvh] ";
 
 const formSchema = toTypedSchema(
@@ -30,7 +27,7 @@ const formSchema = toTypedSchema(
   }),
 );
 
-const { isFieldDirty, handleSubmit, isSubmitting, setValues } = useForm({
+const { isFieldDirty, handleSubmit, isSubmitting, setValues, meta } = useForm({
   validationSchema: formSchema,
 });
 
@@ -65,22 +62,26 @@ onMounted(() => {
       class="absolute right-0 top-0 -z-10 w-full h-full bg-[#3E5A85] sm:bg-emerald-500"
     >
       <img
-        src="/bg-img/login.jpg"
+        src="@/assets/images/login-bg.jpg"
         alt=""
         class="h-screen w-full object-cover hidden sm:block opacity-85"
       />
     </div>
     <main :class="mainHeight + 'grid place-items-center py-10'">
       <form
-        class="flex flex-col items-center gap-5 rounded-3xl bg-white p-10 text-3xl font-normal"
+        class="flex flex-col items-center gap-8 rounded-3xl bg-white px-16 py-10 text-3xl font-normal"
         @submit="onSubmit"
       >
-        <img :src="schoolInfo.logo" />
-        <h1 class="hidden text-center uppercase sm:block">
-          Registro de datos de autorización
+        <figure
+          class="h-[208px] w-[267px] bg-[#D9D9D9] grid place-items-center rounded-[20px]"
+        >
+          <img src="@/assets/images/logo-form.png" />
+        </figure>
+        <h1 class="hidden text-center sm:block text-[32px] font-medium mt-3">
+          Registro de datos de Autorización
         </h1>
-        <section class="flex flex-col gap-4">
-          <p>Inicio de sesión</p>
+        <section class="flex flex-col gap-6 w-[298px]">
+          <p class="text-[25px] text-center mb-4">Inicio de sesión</p>
           <FormField
             v-slot="{ componentField }"
             name="email"
@@ -92,6 +93,7 @@ onMounted(() => {
                   v-bind="componentField"
                   type="email"
                   placeholder="Email"
+                  class="h-[46px]"
                 />
               </FormControl>
               <FormMessage />
@@ -108,12 +110,17 @@ onMounted(() => {
                   v-bind="componentField"
                   type="password"
                   placeholder="Contraseña"
+                  class="h-[46px]"
                 />
               </FormControl>
               <FormMessage />
             </FormItem>
           </FormField>
-          <Button type="submit" :disabled="isSubmitting" class="bg-[#00635D]">
+          <Button
+            type="submit"
+            :disabled="isSubmitting || !meta.valid"
+            class="bg-[#00635D] disabled:opacity-50 hover:bg-[#003330] self-center px-9 text-base font-normal h-[45px] mt-2 mb-8"
+          >
             <template v-if="isSubmitting">
               <LoaderCircle class="w-4 h-4 mr-2 animate-spin" />
               Iniciando Sesión
@@ -127,7 +134,7 @@ onMounted(() => {
           >
             <label
               for="rememberPassword"
-              class="flex w-full items-center justify-between text-base"
+              class="flex w-56 items-center justify-between text-base self-center"
             >
               <p class="select-none">Recordar contraseña</p>
               <Checkbox
@@ -140,7 +147,7 @@ onMounted(() => {
           </FormField>
           <a
             href="/recoverPassword"
-            class="mt-5 text-center text-sm text-black underline decoration-2"
+            class="mt-5 text-center text-sm text-black underline decoration-1"
             >¿Olvidaste tu contraseña?</a
           >
         </section>
@@ -155,5 +162,9 @@ body,
   height: 100%;
   min-height: 100vh;
   min-height: 100dvh;
+}
+
+figure {
+  box-shadow: 0px 4px 4px 0px #00000040;
 }
 </style>
