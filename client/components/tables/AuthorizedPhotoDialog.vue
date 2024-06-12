1fe13fc3f0c2ec798ba42284dd7ab8e445668be7
Nicolas <nicolasgalarza40@gmail.com>
2024-06-12 10:36:00 -0300

    fix(frontend): authorizeds image fixed in director's view

--- client/components/tables/AuthorizedPhotoDialog.vue
+++ client/components/tables/AuthorizedPhotoDialog.vue
@@ -6,57 +6,32 @@
           <LoaderCircle class="animate-spin" :size="40" />
         </div>
       </template>
-      <template v-else>
-        <img class="aspect-square w-full animate-in" :src="img" />
-      </template>
+      <img
+        v-show="!loading"
+        class="aspect-square w-full animate-in"
+        :src="imageSrc"
+        @load="loading = false"
+      />
     </DialogContent>
   </Dialog>
 </template>
 <script setup lang="ts">
 import { LoaderCircle } from "lucide-vue-next";
-import { toast } from "vue-sonner";
 import Dialog from "@/components/ui/dialog/Dialog.vue";
 import DialogContent from "@/components/ui/dialog/DialogContent.vue";
-import api from "@/utils/authAxios";
 
+const config = useRuntimeConfig();
 const props = defineProps<{
   photo: string;
 }>();
 
 const visibility = defineModel<boolean>("visibility", { default: false });
-const img: Ref<string> = ref("");
 const loading: Ref<boolean> = ref(true);
+const imageSrc: ComputedRef<string> = computed(
+  () => config.public.baseApiUrl + "/images/" + props.photo,
+);
 
-async function getImage() {
-  const { data, headers } = await api.get("/api/image/" + props.photo, {
-    responseType: "arraybuffer",
-  });
-
-  const base64 = encodeToBase64(data);
-
-  return `data:${headers["Content-Type"]};base64,${base64}`;
-}
-
-function encodeToBase64(data: Iterable<number>): string {
-  return btoa(
-    new Uint8Array(data).reduce(
-      (_data, byte) => _data + String.fromCharCode(byte),
-      "",
-    ),
-  );
-}
-
-watch(visibility, async (v) => {
-  if (v) {
-    img.value = "";
-    loading.value = true;
-    try {
-      img.value = await getImage();
-    } catch (e) {
-      toast.error("Ocurrió un error y no se pudo cargar la imagen");
-    } finally {
-      loading.value = false;
-    }
-  }
+watch(visibility, (v) => {
+  if (v) loading.value = true;
 });
 </script>
